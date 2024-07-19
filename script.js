b30091f0a3266742a85d52a624e9783343747fe2
dibesfer <dibesfer@gmail.com>
2024-07-19 17:47:27 +0200

    dark theme

---
+++ script.js
@@ -0,0 +1,3 @@
+var header = document.getElementsByTagName("HEADER")
+
+header[0].innerHTML = '<a href="/" style="color: unset"><img class="pixel icon" style="display:block; margin:auto" width="400px"src="/assets/aeshaedi_title.png" alt="Ashaedi title in pixel"></a>'
